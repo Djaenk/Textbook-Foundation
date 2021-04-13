44c36da368779432fbf7dceed87b93a60d099a14
Djaenk <wanganthony.wang@gmail.com>
2021-04-13 18:25:28 -0500

    Programmed home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
+import { Home } from './Home';
 import axios from 'axios';
 
 // React functional component
@@ -68,18 +69,7 @@ function App () {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <button onClick={fetchBase} style={{marginBottom: '1rem'}}> {`GET: http://${url}:8000/`} </button>
-        <button onClick={reset}> Reset DB </button>
-        <form onSubmit={handleSubmit}>
-          <input type="text" value={number} onChange={handleChange}/>
-          <br/>
-          <input type="submit" value="Submit" />
-        </form>
-        <ul>
-          { values.map((value, i) => <li key={i}>{value.value}</li>) }
-        </ul>
-      </header>
+      <Home/>
     </div>
   );
 }
