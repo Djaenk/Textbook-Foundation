7caf13428dbc82430b867c90c9a4d3a365dbaeaf
KirkWThomas <kirkt@smu.edu>
2021-04-21 10:04:36 -0500

    added links from AllBooks to book page

--- frontend/src/Routes.jsx
+++ frontend/src/Routes.jsx
@@ -14,7 +14,7 @@ export default function Routes({ appProps }) {
         <Route path="/" exact component={Landing} appProps={appProps} />
         <Route path="/home" exact component={Home} appProps={appProps} />
         <Route path="/register" exact component={Register} appProps={appProps} />
-	<Route path="/books/:id" component={Book} appProps={appProps} />
+        <Route path="/books/:id" component={Book} appProps={appProps} />
         {/* <Route path="/login" exact component={Login} appProps={appProps} /> */}
         {/* <Route
           path="/register"
